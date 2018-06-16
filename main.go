dd23c0d92c15bd1c8d9d90c45f4525b7457bf3de
Collins <collinsdelan@gmail.com>
2018-06-16 17:15:21 +0300

    add create book func

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -39,6 +41,15 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(97999))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 func main() {
